7d734c624adea71a7ac524d56ea980f7df4981a6
Igor Checheurov <chei@yandex.ru>
2021-08-14 00:59:51 +0300

    fix: correct formula again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
 
   let numReading = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let numTenthReading = ['', '', 'tventy', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let numTenthReading = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   let numRankReading = ['hundred', 'thousand', 'million', 'billion'];
   let reading = '';
   let thousands = 0;
@@ -20,12 +20,15 @@ module.exports = function toReadable (number) {
   }
 
   if (number < 1000) {
-    return `${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] : numTenthReading[(Math.floor(number/10) % 10)] + " " + numReading[(number % 10)]}`;
+    return `${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] :
+       (numTenthReading[(Math.floor(number/10) % 10)] + ((number % 10) > 0) ? " " + numReading[(number % 10)] : "")}`;
   }
 
   if (number < 100000) {
-    thousands = number / 1000;
-    thousandString = `${numTenthReading[(Math.floor(thousands/10) % 10)]} ${numReading[(thousands % 10)]}`;
-    return `${thousandString} thousand ${numReading[Math.floor(number/100)]} hundred ${numTenthReading[(Math.floor(number/10) % 10)]} ${numReading[(number % 10)]}`;
+    thousands = Math.floor(number / 1000);
+    thousandString = `${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] :
+      (numTenthReading[(Math.floor(number/10) % 10)] + ((number % 10) > 0) ? " " + numReading[(number % 10)] : "")}`;
+    return `${thousandString} thousand ${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] :
+      (numTenthReading[(Math.floor(number/10) % 10)] + ((number % 10) > 0) ? " " + numReading[(number % 10)] : "")}`;
   }
 }
