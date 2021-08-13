0a3e97f628c8bbb9393c1ce3c564861d8582e4e3
Igor Checheurov <chei@yandex.ru>
2021-08-14 00:26:57 +0300

    fix: correct formula again

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 module.exports = function toReadable (number) {
 
-  let numReading = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  let numTenthReading = ['ten', 'tventy', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  let numReading = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  let numTenthReading = ['', 'tventy', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
   let numRankReading = ['hundred', 'thousand', 'million', 'billion'];
   let reading = '';
   let thousands = 0;
@@ -16,16 +16,16 @@ module.exports = function toReadable (number) {
   if (number < 20) return numReading[number];
 
   if (number < 100) {
-    return `${numTenthReading[Math.floor(number/10)]} ${numReading[(number % 10)]}`
+    return `${numTenthReading[Math.floor(number/10)]} ${numReading[(number % 10)]}`;
   }
 
   if (number < 1000) {
-    return `${numReading[Math.floor(number/100)]} hundred ${numTenthReading[(Math.floor(number/10) % 10)]} ${numReading[(number % 10)]}`
+    return `${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number] : numTenthReading[(Math.floor(number/10) % 10)] + " " + numReading[(number % 10)]}`;
   }
 
   if (number < 100000) {
     thousands = number / 1000;
-    thousandString = `${numTenthReading[(Math.floor(thousands/10) % 10)]} ${numReading[(thousands % 10)]}`
-    return `${thousandString} thousand ${numReading[Math.floor(number/100)]} hundred ${numTenthReading[(Math.floor(number/10) % 10)]} ${numReading[(number % 10)]}`
+    thousandString = `${numTenthReading[(Math.floor(thousands/10) % 10)]} ${numReading[(thousands % 10)]}`;
+    return `${thousandString} thousand ${numReading[Math.floor(number/100)]} hundred ${numTenthReading[(Math.floor(number/10) % 10)]} ${numReading[(number % 10)]}`;
   }
 }
