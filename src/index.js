de947f618d08f299326560fcc3644d05d04a0987
My Name <my@test.com>
2021-08-14 13:40:56 +0300

    feat: add function to convert tens to text

--- src/index.js
+++ src/index.js
@@ -5,30 +5,38 @@ module.exports = function toReadable (number) {
   let numRankReading = ['hundred', 'thousand', 'million', 'billion'];
   let reading = '';
   let thousands = 0;
-  let millions = 0;
-  let billions = 0;
+  let tens = 0;
+  let hundreds = 0;
   let thousandString = '';
-  let millionString = '';
-  let billionString = '';
+  let tensString = '';
+  let hundredString = '';
 
-  if (number === undefined) return '';
+  function turnTensToString(hundreds) {
+    if (hundreds === 0) return '';
+    tens = hundreds>100 ? hundreds % 100 : hundreds;
+    tensString = tens < 20 ? numReading[tens] :
+      numTenthReading[(Math.floor(tens/10)] + ((tens % 10) > 0 ?
+       ' ' + numReading[(tens % 10)] : '');
+    return tensString;
+  }
 
-  if (number < 20) return numReading[number];
+  if (number === undefined) return ('');
+
+  if (number < 20) return (numReading[number]);
 
   if (number < 100) {
-    return `${numTenthReading[Math.floor(number/10)]} ${numReading[(number % 10)]}`;
+    return (`${numTenthReading[Math.floor(number/10)]} ${numReading[(number % 10)]}`);
   }
 
   if (number < 1000) {
-    return `${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] :
-       numTenthReading[(Math.floor(number/10) % 10)] + ((number % 10) > 0) ? ' ' + numReading[(number % 10)] : ''}`;
+    return (`${numReading[Math.floor(number/100)]} hundred ${turnTensToString(number)}`);
   }
 
-  if (number < 100000) {
+  if (number < 1000000) {
     thousands = Math.floor(number / 1000);
-    thousandString = `${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] :
-       numTenthReading[(Math.floor(number/10) % 10)] + ((number % 10) > 0) ? ' ' + numReading[(number % 10)] : ''}`;
-    return `${thousandString} thousand ${numReading[Math.floor(number/100)]} hundred ${(number % 100) < 20 ? numReading[number % 100] :
-       numTenthReading[(Math.floor(number/10) % 10)] + ((number % 10) > 0) ? ' ' + numReading[(number % 10)] : ''}`;
+    thousandString = `${numReading[Math.floor(thousands/100)]} hundred ${turnTensToString(thousands)} thousand `;
+    hundreds = number % 1000;
+    hundredString = `${numReading[Math.floor(hundreds/100)]} hundred ${turnTensToString(hundreds)}`;
+    return (thousandString + hundredString);
   }
 }
